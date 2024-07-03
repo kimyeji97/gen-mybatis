5ec8e54cae0838f34716fbf5ae2fe20ba352ae5d
lilly <kimyeji2035@gmail.com>
2024-07-03 10:38:10 +0900

    remove .idea in  git repository

--- gen_mybatis-작업중.py
+++ gen_mybatis-작업중.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import sys , os, io ,re
+import sys, os, io, re
 import datetime
 
 from gen_config import FIELD_NAME_ENUM_TYPES, ENUM_PACKAGE, con_opts, con_schema
+
 
 class PackagePathInfo:
     def __init__(self, xml_path, mapper_path, model_path, gen_domain_package, base_domain_path):
@@ -14,7 +15,11 @@ class PackagePathInfo:
         self.gen_domain_package = gen_domain_package
         self.base_domain_path = base_domain_path
 
+
 class ColumnInfo:
+    def __init__(self):
+        pass
+
     def __int__(self, is_remove_cd, is_remove_yn, is_use_date_format, is_use_time_format, base_domain_colums):
         self.is_remove_cd = is_remove_cd
         self.is_remove_yn = is_remove_yn
@@ -23,42 +28,48 @@ class ColumnInfo:
         self.base_domain_colums = base_domain_colums
 
 
-
-__IS_VERSION_3__= sys.version_info.major == 3
+__IS_VERSION_3__ = sys.version_info.major == 3
 _IS_WINDOW_ = os.name == 'Windows' or os.name == 'nt'
 
-SP4  = ' '*4
-SP8  = ' '*8
-SP12 = ' '*12
+SP4 = ' ' * 4
+SP8 = ' ' * 8
+SP12 = ' ' * 12
 
+_package_path_info = None
+_column_info = None
 
-_package_path_info=None
-_column_info=None
 
 def set_base_info(p_info, c_info):
     _package_path_info = p_info
     _column_info = c_info
 
-def rreplace(s, old, new, occurrence):
+
+def replace(s, old, new, occurrence):
     li = s.rsplit(old, occurrence)
     return new.join(li)
+
 
 def to_field_name(value):
     value = value.replace("tb_", "", 1)
     value = value.replace("TB_", "", 1)
+
     def camelcase():
         yield str.lower
         while True:
             yield str.capitalize
+
     c = camelcase()
     if __IS_VERSION_3__:
-        return "".join(c.__next__()(x) if x else '_' for x in re.split('[-_ ]',value))
+        return "".join(c.__next__()(x) if x else '_' for x in re.split('[-_ ]', value))
     else:
-        return "".join(c.next()(x) if x else '_' for x in re.split('[-_ ]',value))
+        return "".join(c.next()(x) if x else '_' for x in re.split('[-_ ]', value))
+
 
 def bool_str(boo):
-    if boo : return 'true'
-    else : return 'false'
+    if boo:
+        return 'true'
+    else:
+        return 'false'
 
 
 def to_class_name(value):
@@ -66,7 +77,7 @@ def to_class_name(value):
     return tmpval[0].upper() + tmpval[1:]
 
 
-def get_model_code(table,fields , mapper_package, model_gen_package):
+def get_model_code(table, fields, mapper_package, model_gen_package):
     tname = table.table_name
     table_class_name = to_class_name(tname) + 'Core'
     # table_field_name = to_field_name(tname)
@@ -74,7 +85,7 @@ def get_model_code(table,fields , mapper_package, model_gen_package):
     source_prefix.append("package {};".format(model_gen_package))
     source_prefix.append("")
     source_prefix.append("import com.innerwave.core.domain.Column;")
-    source_prefix.append("import "+ _package_path_info.base_domain_path + ";")
+    source_prefix.append("import " + _package_path_info.base_domain_path + ";")
     source_prefix.append("import lombok.ToString;")
 
     source = []
@@ -101,9 +112,9 @@ def get_model_code(table,fields , mapper_package, model_gen_package):
                 prop_str.append('value = "{}"'.format(field.jackson_prop['value']))
             if 'access' in field.jackson_prop:
                 prop_str.append('access = {}'.format(field.jackson_prop['access']))
-            source.append('    @JsonProperty({})'.format(','.join(prop_str) ) )
+            source.append('    @JsonProperty({})'.format(','.join(prop_str)))
 
-        if(_column_info.is_use_date_format):
+        if (_column_info.is_use_date_format):
             if field.type.startswith("date") or field.type.startswith("timestamp"):
                 imp_str = "import org.springframework.format.annotation.DateTimeFormat;"
                 if imp_str not in source_prefix:
@@ -111,30 +122,30 @@ def get_model_code(table,fields , mapper_package, model_gen_package):
                 imp_str = "import com.fasterxml.jackson.annotation.JsonFormat;"
                 if imp_str not in source_prefix:
                     source_prefix.append(imp_str)
-        elif(_column_info.is_use_date_format is False and _column_info.is_use_time_format):
+        elif (_column_info.is_use_date_format is False and _column_info.is_use_time_format):
             if field.type == 'time':
                 imp_str = "import com.fasterxml.jackson.annotation.JsonFormat;"
                 if imp_str not in source_prefix:
                     source_prefix.append(imp_str)
 
-        if(_column_info.is_use_date_format):
+        if (_column_info.is_use_date_format):
             if field.type.startswith("datetime(3") or field.type.startswith("timestamp(3"):
                 source.append('    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss.SSS")')
                 source.append('    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss.SSS")')
             elif field.type == 'date':
                 source.append('    @DateTimeFormat(pattern="yyyy-MM-dd")')
                 source.append('    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")')
-            elif field.type == 'datetime'or field.type == 'timestamp':
+            elif field.type == 'datetime' or field.type == 'timestamp':
                 source.append('    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")')
                 source.append('    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")')
 
-
-        if(_column_info.is_use_time_format):
+        if (_column_info.is_use_time_format):
             if field.type == 'time':
                 source.append('    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm:ss")')
 
         # source.append('    @Column(columnName = "{}", type = "{}", length = {}, nullable = {})'.format(f, field.type, f.))
-        source.append('    @Column(columnName = "{}", type = "{}", nullable = {}, primary = {})'.format(f, field.type, bool_str(field.nullable), bool_str(field.is_pk)))
+        source.append('    @Column(columnName = "{}", type = "{}", nullable = {}, primary = {})'.format(f, field.type, bool_str(field.nullable),
+                                                                                                        bool_str(field.is_pk)))
         source.append("    private {} {};".format(field.java_type, field.java_field_name))
 
     for field in fields:
@@ -157,7 +168,7 @@ def get_model_code(table,fields , mapper_package, model_gen_package):
     }}"""
 
         # java_type이 배열이면 get,set clone해서.
-        if java_type.endswith('[]') :
+        if java_type.endswith('[]'):
             get_set_format = """
     public void set{field_t_name}({java_type} {field_f_name})
     {{
@@ -179,9 +190,9 @@ def get_model_code(table,fields , mapper_package, model_gen_package):
     	}}
     }}"""
 
-        get_set = get_set_format.format(field_t_name = field_t_name
-                                        ,field_f_name = field_f_name
-                                        ,java_type = java_type)
+        get_set = get_set_format.format(field_t_name=field_t_name
+                                        , field_f_name=field_f_name
+                                        , java_type=java_type)
         source.append(get_set)
     source.append("}")
 
